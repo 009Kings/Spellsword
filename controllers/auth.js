5c937a2cb50f31709e50a76897aa3f42a49dd15c
009kings <009kings@gmail.com>
2019-01-02 13:03:31 -0800

    passwords and forms and controllers oh my

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,19 @@ var router = express.Router();
 
 // Declare routes
 router.get("/login", (req, res)=>{
-  res.send("hello, login");
+  res.render("auth/login");
+})
+
+router.get("/signup", (req, res)=>{
+  res.render("auth/signup");
+})
+
+router.post("/login", (req, res)=>{
+  res.send("Post to login is working!");
+})
+
+router.post("/signup", (req, res)=>{
+  res.send("Post to signup is working!");
 })
 
 // Export
