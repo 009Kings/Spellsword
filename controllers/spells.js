a775977a42c8b45f15155673cc00ea198f9aa261
009kings <009kings@gmail.com>
2019-01-16 18:59:31 -0800

    Views show more stuff

--- controllers/spells.js
+++ controllers/spells.js
@@ -3,11 +3,26 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/', (req, res)=>{
-  res.send("Spells");
+  db.spell.findAll().then((spells)=>{
+    res.render("spells/spellList", {spells: spells});
+  }).catch(err=>{
+    console.log(`Bad news bears! There's neen an error getting all the spells!`);
+    console.log(err);
+    res.render("error");
+  })
 })
 
 router.get('/:id', (req, res)=>{
-  res.send(`Showing spell at id ${req.params}`);
+  db.spell.findOne({
+    where: {id: req.params.id},
+    include: [db.school]
+  }).then(spell=>{
+    res.render("spells/showSpell", { spell : spell });
+  }).catch(err=>{
+    console.log(`Bad news bears! There's neen an error getting all the spells!`);
+    console.log(err);
+    res.render("error");
+  })
 })
 
 module.exports = router;
